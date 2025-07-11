c9308534b8b0dbb73f6628bfdc0d798d1ee760e7
brad-dallin <brad.dallin@gmail.com>
2025-07-10 21:37:11 -0400

    Refactor stylize_binding_site function to include surface parameter and improve surface styling options

--- examples/binding-site/binding-site.py
+++ examples/binding-site/binding-site.py
@@ -11,23 +11,28 @@ from pymol import cmd
 
 ## Functions
 def stylize_binding_site(
-        selection1: str,
-        selection2: str = "polymer") -> None:
+                selection1: str,
+                selection2: str = "polymer",
+                surface: bool = 0) -> None:
     """
     """
-    cmd.set("surface_carve_cutoff", 4.5)
-    cmd.set("surface_carve_selection", selection1)
-    cmd.set("surface_carve_normal_cutoff", -0.1)
+    cmd.hide("everything")
+    cmd.show("cartoon")
+    cmd.color("white", "polymer")
+    cmd.color("atomic", "not elem C")
+    if surface > 0:
+        cmd.set("surface_type", 2)
+        cmd.set("two_sided_lighting")
+        cmd.set("surface_carve_cutoff", 4.5)
+        cmd.set("surface_carve_normal_cutoff", -0.1)
+        cmd.set("surface_carve_selection", selection1)
+        cmd.show("surface", f"byres {selection2} within 8 of {selection1}")
+        cmd.set("transparency", 0.5, selection2)
+        cmd.set("surface_color", "white", selection2)
+        cmd.unset("ray_shadows")
 
-    cmd.show("surface", f"{selection2} within 8 of {selection1}")
-    cmd.set("two_sided_lighting")
-    cmd.set("transparency", 0.5)
     cmd.show("sticks", selection1)
-    cmd.orient(selection1)
-
-    # cmd.set("surface_color", "white")
-    cmd.set("surface_type", 2)
-    cmd.unset("ray_shadows")
+    cmd.show("sticks", f"byres {selection2} within 3.5 of {selection1}")
 
 ## Extend PyMOL commands
 cmd.extend("stylize_binding_site", stylize_binding_site)
